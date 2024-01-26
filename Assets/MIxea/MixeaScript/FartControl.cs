c9a728978d0f5dd1d39a7900b9aba82c3529e038
Carlos Michea A <72899324+CarlosMichea@users.noreply.github.com>
2024-01-26 20:09:04 -0300

    Womba listo, Peo a mitad
    #eso

---
+++ Assets/MIxea/MixeaScript/FartControl.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FartControl : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private bool moveRight;
+
+    private Transform floorDetect1;
+    private Transform floorDetect2;
+
+    private bool farting;
+    [SerializeField] private float fartTimer;
+    private float fartTimerCurrent;
+    [SerializeField] private float fartStartup;
+    private float fartStartupCurrent;
+    [SerializeField] private float restWait;
+    private float restWaitCurrent;
+
+    private bool fartCreated;
+
+
+    void Start()
+    {
+        moveRight = true;
+
+        floorDetect1 = gameObject.transform.Find("Floor Detect 1");
+        floorDetect2 = gameObject.transform.Find("Floor Detect 2");
+
+        farting = false;
+        fartTimerCurrent = fartTimer;
+        fartCreated = false;
+    }
+
+    void Update()
+    {
+        AvoidFall();
+    }
+
+    private void FixedUpdate()
+    {
+        if(farting == false)
+        {
+            FartCountdown();
+        }
+        else
+        {
+            Farting();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            moveRight = !moveRight;
+        }
+
+        if (col.gameObject.CompareTag("Player"))
+        {
+            //Destroy(gameObject);
+        }
+    }
+
+    void AvoidFall()
+    {
+        //Floor detection
+        RaycastHit2D hit = Physics2D.Raycast(floorDetect1.transform.position, Vector2.down, 1);
+        RaycastHit2D hit2 = Physics2D.Raycast(floorDetect2.transform.position, Vector2.down, 1);
+        Debug.DrawRay(floorDetect1.transform.position, Vector2.down, Color.green);
+        Debug.DrawRay(floorDetect2.transform.position, Vector2.down, Color.green);
+
+        if (hit.collider == null || hit2.collider == null)
+        {
+            //Change directions
+            moveRight = !moveRight;
+        }
+    }
+
+    void Moving()
+    {
+        if (moveRight)
+        {
+            transform.Translate(Time.deltaTime * speed, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-1 * Time.deltaTime * speed, 0, 0);
+        }
+    }
+
+    void FartCountdown()
+    {
+        if (fartTimerCurrent >= 0)
+        {
+            Moving();
+            fartTimerCurrent -= Time.deltaTime;
+        }
+        else
+        {
+            //Reset Farting Timers
+            fartStartupCurrent = fartStartup;
+            restWaitCurrent = restWait;
+            //Start farting
+            farting = true;
+        }
+    }
+
+    void Farting()
+    {
+        //FartCharge time
+        if (fartStartupCurrent <= 0)
+        {
+            //Resting after fart time
+            if(restWaitCurrent <= 0)
+            {
+                //Start FartCountdown()
+                fartTimerCurrent = fartTimer;
+                farting = false;
+                fartCreated = false;
+            }
+            else
+            {
+                //Create fart cloud once
+                if (fartCreated == false)
+                {
+                    fartCreated = true;
+
+                    //Put fart code here
+                }
+
+                //Timer
+                restWaitCurrent -= Time.deltaTime;
+                Debug.Log("resting");
+            }
+        }
+        else
+        {
+            //Timer
+            fartStartupCurrent -= Time.deltaTime;
+            Debug.Log("farting");
+        }
+    }
+
+}
---
+++ Assets/MIxea/MixeaScript/GombaControl.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GombaControl : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private bool moveRight;
+
+    private Transform floorDetect1;
+    private Transform floorDetect2;
+
+    void Start()
+    {
+        moveRight = true;
+
+        floorDetect1 = gameObject.transform.Find("Floor Detect 1");
+        floorDetect2 = gameObject.transform.Find("Floor Detect 2");
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Moving();
+        AvoidFall();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            moveRight = !moveRight;
+        }
+
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void AvoidFall()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(floorDetect1.transform.position, Vector2.down, 1);
+        RaycastHit2D hit2 = Physics2D.Raycast(floorDetect2.transform.position, Vector2.down, 1);
+        Debug.DrawRay(floorDetect1.transform.position, Vector2.down, Color.green);
+        Debug.DrawRay(floorDetect2.transform.position, Vector2.down, Color.green);
+
+        if (hit.collider == null || hit2.collider == null)
+        {
+            moveRight = !moveRight;
+        }
+    }
+
+    void Moving()
+    {
+        if (moveRight)
+        {
+            transform.Translate(Time.deltaTime * speed, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-1 * Time.deltaTime * speed, 0, 0);
+        }
+    }
+}
