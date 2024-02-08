f0eeb9eda6aab70e838772f691732f717f2ce3d9
Carlos Michea A <72899324+CarlosMichea@users.noreply.github.com>
2024-02-08 17:32:48 -0300

    Death Controller

---
+++ Assets/MIxea/MixeaScript/DeathController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathController : MonoBehaviour
+{
+    private BoxCollider2D[] selfColliders;
+    private Rigidbody2D rb;
+    [SerializeField] private Behaviour disableScript;
+
+    private void Start()
+    {
+        selfColliders = gameObject.GetComponents<BoxCollider2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //Testing
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            DeathSelf();
+        }
+    }
+
+    public void DeathSelf()
+    {
+        //Disable Colliders
+        foreach (var col in selfColliders)
+        {
+            col.enabled = false;
+        }
+
+        rb.gravityScale = 0f;
+        disableScript.enabled = false;
+    }
+}
